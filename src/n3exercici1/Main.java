a1ea027440ca9c8f0896ed4180ca6e6fb6daa10c
kanigula162 <kanigula@msn.com>
2023-02-04 21:52:36 +0100

    Actualització del menú de l'aplicació

--- src/n3exercici1/Main.java
+++ src/n3exercici1/Main.java
@@ -1,6 +1,5 @@
 package n3exercici1;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -18,14 +17,22 @@ public class Main {
             System.out.println("6.- Calcular puntuació de la notícia.");
             System.out.println("7.- Calcular preu-notícia.");
             System.out.println("8.- Sortir.");
-            Scanner sc = new Scanner(System.in);
+
             try{
+                Scanner sc = new Scanner(System.in);
                 seleccio = Integer.parseInt(sc.next());
+
+                if(seleccio < 1 || seleccio > 8){
+                    System.out.println("\n>>> Seleccioni un valor del 1 al 8 siusplau <<<\n");
+                }
 
             }catch (Exception e){
                 seleccio = 0;
+                System.out.println("\n>>> Seleccioni un valor numèric del 1 al 8 siusplau <<<\n");
             }
         }while(seleccio != 8);
 
+        System.out.println("\n#########> FI DEL PROGRAMA <##########");
+        System.exit(0);
     }
 }
